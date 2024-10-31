31b7cb5744ea1c6e4c093494f3662abe59368d93
LAPTOP-JJ518A0L\minht <minhthanh5346@gmail.com>
2024-11-01 00:30:17 +0700

    search v0.1

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const AuthenticateRouter = require("./authenticate");
 const ProductRouter = require("./product");
+const UserRouter = require("./user");
 
 const router = express.Router();
 
 // Register routers
 router.use("/auth", AuthenticateRouter);
 router.use("/products", ProductRouter);
+router.use("/users", UserRouter);
 
 module.exports = router;
