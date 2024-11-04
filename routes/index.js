9f53deb05e14dfbd5f4135980299c386762d01e3
nguyentaan <vantaan2002@gmail.com>
2024-11-04 21:37:01 +0700

    Model and Controller Order

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 // const UserRouter = require("./user");
 const AuthenticateRouter = require("./authenticate");
 const ProductRouter = require("./product");
+const OrderRouter = require('./orderRoutes');
 
 // const DocumentRouter = require("./document");
 // const DocumentTypeRouter = require("./document_type");
@@ -10,6 +11,7 @@ const routes = (app) => {
 //   app.use("/user", UserRouter);
   app.use("/auth", AuthenticateRouter);
   app.use("/products", ProductRouter);
+  app.use("/orders", OrderRouter);
 //   app.use("/knowledge_base", KnowledgeBaseRouter);
 //   app.use("/document", DocumentRouter);
 //   app.use("/document_type", DocumentTypeRouter);
