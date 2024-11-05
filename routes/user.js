e1b26e199d9ac3c6105038a471f46002ea95c2ba
LAPTOP-JJ518A0L\minht <minhthanh5346@gmail.com>
2024-11-05 23:43:57 +0700

    keeping change

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,6 @@ const userController = require("../controllers/user/user.js");
 
 // const { authCheckToken } = require("../middleware/checkToken.js");
 
-// router.get("/", authCheckToken, productController.getAllProduct);
 router.put("/keyword/add", userController.UpdateSearchHistory);
 
 module.exports = router; // Change this line to use CommonJS syntax
